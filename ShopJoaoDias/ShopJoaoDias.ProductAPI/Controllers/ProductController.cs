4e47e3f798fc53674ad9c55145c6482d9239c28e
JoaoDias <joaodiasworking@gmail.com>
2022-04-14 22:29:16 -0300

    ProductController e Seeds implementados

---
+++ ShopJoaoDias/ShopJoaoDias.ProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopJoaoDias.ProductAPI.Data.ValueObjects;
+using ShopJoaoDias.ProductAPI.Repository;
+
+namespace ShopJoaoDias.ProductAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private IProductRepository _repository;
+
+        public ProductController(IProductRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductVO>>> FindAll()
+        {
+            var products = await _repository.FindAll();
+            return Ok(products);
+        }
+
+        [HttpGet("{id:long}")]
+        public async Task<ActionResult<ProductVO>> FindById(long id)
+        {
+            var product = await _repository.FindById(id);
+            if (product == null) return NotFound();
+            return Ok(product);
+            {
+
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ProductVO>> FindById(ProductVO productVo)
+        {
+            if (productVo == null) return BadRequest();
+            var product = await _repository.Create(productVo);
+            return Ok(product);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<ProductVO>> Update(ProductVO productVo)
+        {
+            if (productVo == null) return BadRequest();
+            var product = await _repository.Update(productVo);
+            return Ok(product);
+        }
+
+        [HttpDelete("{id:long}")]
+        public async Task<ActionResult> Delete(long id)
+        {
+            var status = await _repository.Delete(id);
+            if (!status) return BadRequest();
+            return Ok(status);
+        }
+    }
+}
--- ShopJoaoDias/ShopJoaoDias.ProductAPI/Program.cs
+++ ShopJoaoDias/ShopJoaoDias.ProductAPI/Program.cs
@@ -17,7 +17,9 @@ builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(connecti
 IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
 builder.Services.AddSingleton(mapper);
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
